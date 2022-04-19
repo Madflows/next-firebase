b8c06a46e4bd5d97c3bee79d1eaaa96eadf87bd8
Madflows <lawalfolarin6@gmail.com>
2022-04-19 14:51:58 +0100

    Fixed PostFeed.js

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -1,34 +1,55 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function PostFeed({ posts, admin }){
-    return posts ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin} />) : null;
+export default function PostFeed({ posts, admin }) {
+  return posts
+    ? posts.map((post) => (
+        <PostItem post={post} key={post.slug} admin={admin} />
+      ))
+    : null;
 }
 
-function PostItem({ post }) {
-    // Naive method to calc word count and read time
-    const wordCount = post?.content.trim().split(/\s+/g).length;
-    const minutesToRead = Math.ceil(wordCount / 200); //(wordCount / 100 + 1).toFixed(0);
+function PostItem({ post, admin = false }) {
+  // Naive method to calc word count and read time
+  const wordCount = post?.content.trim().split(/\s+/g).length;
+  const minutesToRead = (wordCount / 100 + 1).toFixed(0);
 
-    return (
-      <div className="card">
-        <Link href={`/${post.username}`}>
-          <a>
-            <strong>By @{post.username}</strong>
-          </a>
-        </Link>
+  return (
+    <div className="card">
+      <Link href={`/${post.username}`}>
+        <a>
+          <strong>By @{post.username}</strong>
+        </a>
+      </Link>
 
-        <Link href={`1/${post.username}/${post.slug}`}>
-          <h2>
-            <a>{post.title}</a>
-          </h2>
-        </Link>
+      <Link href={`1/${post.username}/${post.slug}`}>
+        <h2>
+          <a>{post.title}</a>
+        </h2>
+      </Link>
 
-        <footer>
-          <span>
-            {wordCount} words. {minutesToRead} min read.
-          </span>
-          <span>❤️ {post.heartCount} Hearts</span>
-        </footer>
-      </div>
-    );
+      <footer>
+        <span>
+          {wordCount} words. {minutesToRead} min read.
+        </span>
+        <span className="push-left">❤️ {post.heartCount} Hearts</span>
+      </footer>
+
+      {/* If admin view, show extra controls for user */}
+      {admin && (
+        <>
+          <Link href={`/admin/${post.slug}`}>
+            <h3>
+              <button className="btn-blue">Edit</button>
+            </h3>
+          </Link>
+
+          {post.published ? (
+            <p className="text-success">Live</p>
+          ) : (
+            <p className="text-danger">Unpublished</p>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
